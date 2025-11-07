f4989a002edf160e474c9df3c6abe92fc5b989ec
LidaSokha03 <sokha.pn@ucu.edu.ua>
2025-11-07 13:15:14 +0200

    classes

---
+++ bot/core/models/cv.py
@@ -0,0 +1,16 @@
+class CV:
+    def __init__(self, user_id: int, firstname: str, lastname: str, email: str, phone: str, education: str, experience: int, skills: list, languages: list, projects: str):
+        self.user_id = user_id #auto_generated
+        self.firstname = firstname #Lida
+        self.lastname = lastname #Sokha
+        self.email = email #lidasosokha@gmail.com
+        self.phone = phone #0964692379 or +380964692379
+        self.education = education #Bachelor's Degree in BA, UCU
+        self.experience = experience # 8 (int in years)
+        self.skills = skills #for the start only in text but after this list of skills ['skill1', 'skill2']
+        self.languages = languages #text for start but after this list of languages ['language1, level', 'language2, level']
+        self.projects = projects #text
+
+     
+        
+
---
+++ bot/core/models/user.py
@@ -0,0 +1,4 @@
+class User:
+    def __init__(self, tg_username: str):
+        self.tg_username = tg_username
+        self.is_registered = False
