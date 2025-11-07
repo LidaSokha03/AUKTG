fe7fed65e065e84415da54afa4baf03d5bbb94b3
LidaSokha03 <sokha.pn@ucu.edu.ua>
2025-11-07 13:30:40 +0200

    mongodb here

---
+++ bot/core/database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB = os.getenv("MONGO_DB")
+
+client = MongoClient(MONGO_URI)
+db = client[MONGO_DB]
+users = db["users"]
+profiles = db["profiles"]
+
+
+print("✅ Колекції:", db.list_collection_names())
