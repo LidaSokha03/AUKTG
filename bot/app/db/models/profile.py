1d14188586c637c07af7b686fa32812aef68d210
mikhailobrytan9@gmail.com <mikhailobrytan9@gmail.com>
2025-11-07 17:37:35 +0200

    Based UI + Some of bonus task

--- bot/app/db/models/profile.py
+++ bot/app/db/models/profile.py
@@ -1,8 +1,50 @@
+from app.db.database import db
+
+class CV:
+    def __init__(self, firstname="", lastname="", email="", phone="", education=None, experience=None, skills=None, languages=None, projects=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.phone = phone
+        self.education = education or []
+        self.experience = experience or []
+        self.skills = skills or []
+        self.languages = languages or []
+        self.projects = projects or []
+
 class Profile:
-    def __init__(self, user_id: int, tg_username: str, fullname: str, email: str, portfolio: str, education: list, skills: list, languages: list):
-        self.user_id = user_id
-        self.tg_username = tg_username
+    def __init__(self, tg_id, fullname="", email="", cv=None):
+        self.tg_id = tg_id
         self.fullname = fullname
         self.email = email
-        self.cv = None
-        self.portfolio = portfolio
+        self.cv = cv or CV()
+
+    def save(self):
+        db.profiles.update_one(
+            {"tg_id": self.tg_id},
+            {
+                "$set": {
+                    "fullname": self.fullname,
+                    "email": self.email,
+                    "cv": {
+                        "firstname": self.cv.firstname,
+                        "lastname": self.cv.lastname,
+                        "email": self.cv.email,
+                        "phone": self.cv.phone,
+                        "education": self.cv.education,
+                        "experience": self.cv.experience,
+                        "skills": self.cv.skills,
+                        "languages": self.cv.languages,
+                        "projects": self.cv.projects,
+                    },
+                }
+            },
+            upsert=True,
+        )
+    @staticmethod
+    def save_template(tg_id, template):
+        db.profiles.update_one(
+            {"tg_id": tg_id},
+            {"$set": {"template": template}},
+            upsert=True
+        )
--- bot/bot1.py
+++ bot/bot1.py
@@ -1,7 +1,9 @@
 from app.bot_instance import bot
-
 import app.handlers.start
-import app.handlers.auth
+import app.handlers.dashboard
+import app.handlers.profile
+import app.handlers.cv_template
+import app.handlers.language
 
 if __name__ == "__main__":
     print("🚀 Bot is running...")
