212c0ba12b6bd944641addc89c0c5a8f07040531
LidaSokha03 <sokha.pn@ucu.edu.ua>
2025-11-08 15:58:25 +0200

    lol

--- bot/bot1.py
+++ bot/bot1.py
@@ -7,9 +7,9 @@ import app.handlers.interview
 import app.handlers.from_pdf
 import app.handlers.dashboard
 import app.handlers.profile
-import app.handlers.cv_template
 import app.handlers.language
 import app.handlers.clear_history
+import app.handlers.cv_history
 
 
 
